d72250d8894d17740e1a2b527b847c10f10ab127
ahsanaliSWE <ahsanalime3@gmail.com>
2024-02-22 12:48:29 +0500

    change

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+
 const express = require('express');
 const admin = require('firebase-admin');
 require('dotenv').config();
 const app = express();
 
+const serviceAccountKey = {
+  "type": "service_account",
+  "project_id": "muet-api",
+  "private_key_id": "c229b27a4ebdf26b2c4ee2cc4988ad18fc366694",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC3QmdUNR0FDN3F\n0ZWrQCJHyBncuUCczjwzAVuaC1vMaBW4gdX7s7dBSmHOx5HhTf+uf212A2wlNgWs\ng9+ZPM7cuSYnIwos0I/T6/LHj00QH/eQqVe6ThiEyJLQ1pgqLKDZcZGlSzb6ibXe\nu3h057WU7x7wRKtFjIG/q/cfCCwMep5zRVdinyzfdlz5QnA2r8Id1aCfUAm1LFf4\nSnI3OiHfhXnEIpsj/muU1XgXcpm3wwzXT14rbO9dJWVfysMuC7u0AYp0jnQdwuoC\nKCGGTtHKnkLw6vYUalmGsKrrulYrM9zMGjPEjK4jEl07xWxJQgCTKKWvbkpsBek+\nJYt0jgLrAgMBAAECggEAHgJ1wUhhVC5jMr27cKuz8WVHNunvNr5fBUs3oOYzRinV\nRL2eOPWZEMwJcD8mGGsZb43WcvjkgMNz8mg1+XYHugVnxgjq5aPRqbZ9r0g8KmPR\nupLAlvWq9jOSNzis+lsMjA8Y9qR2gFiEu9CfTSWUiqsNufLEyULVDHowoZhI/lHf\nKCfW4UzuD9nGwH/a7B1HS5KbHzoFTuwndSf8tWaSubufwBtDsTFu3xnmu72FklSC\nLGW6dixlYuaDgztauwO+Xjt4rmUhqwLTPlcNEZYQsSIheIjaHxdN8ilc6BZmj3CQ\nsMLzhElsk8ICsINDG0VtCdgeZ4V4gLbI1FEyFFWjwQKBgQDZi21BUtuReGSk0lOs\n9BrA2ajJGlQ2JwrPw2c1oxwsZjtKDcaYlOuSHmtiqXVT0flHhtaLS2+zeGSJ3P4l\nP0pRBmaSdsfymEu6gre9OHjNcDCZR1wROAu+rqJZLNWf2OTwqFzDgtHJ5ckZFagU\n1dDhHeGGquXKgIq39SW1/uKRKQKBgQDXp3ZnVBYPshHZHJDKzHPo6j+TEBKFxN1Z\nOxpXpLyJXFKkmlSq9NrvNF5Nx/XMoMDKKK+elu2Yp3+5+no5+dwyDLDYRNEYdyQ3\nMtxqlhYU+YOkQWeE+DJnGAA4VJ3yMteN7sr7Gsct96qgi+BhUFD139RUje9C/hqw\nyCPX/9aR8wKBgQCP6qlQudwAFMGgOHSayrKYOmZrBDwJxzJj2EWDOnD8/nTivNJR\nRtPBStaH4st3tEVRiGaOqE0xUchRXB3VzlT/3VgmNOZ6Gz6eGLQtPhrM7hd4j7jc\nu1MpxqYokHySzw5GF1unC/h1tMqhCa60ee9Bvl7glFaI7RjYMhnMmN6SYQKBgBv5\nH/W1RYtRqNkFyI5VyO88e3SjEBQD/kZV0UvvFXmn3Rx23Ga0jjWbEGR8fJjHfVp7\nvpQOtdEoYPwJzpL4oyRm2wWin5DR24PBeC/nOgQMfZPv2/K2kysmRNkJuNcy2x0R\nDYlW5uFCkAJ1Qmy9E1NhblRteMyy01l9DJE3IWkbAoGBAKQSGZ2yNkkroNOQUqNX\noFnyKeJzo61bNdRy3M3m965aREzC7dqRqOOkyCsIRzqQqa0fHRzZyVd6r82tfFZr\nUJ8mkxoRUN6oe/sTtH3srxNx4kB2PTXsetC8yeKejQyfUGiXPGJ3U9/pM22a+/XD\nh4LB3sggM3J8Af6bsePmnoDU\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-jsu7e@muet-api.iam.gserviceaccount.com",
+  "client_id": "106121180160763375322",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-jsu7e%40muet-api.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+
+
 const PORT = process.env.PORT || 3000;
-const serviceAccount = require(`/Users/Ahsan Ali/Downloads/muet-api-firebase-adminsdk-jsu7e-c229b27a4e.json`);
+//const serviceAccount = require(serviceAccountKey);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // No need for databaseURL when using Firestore
+  credential: admin.credential.cert(serviceAccountKey),
 });
 
+app.use(cors());
 app.use(express.json()); // Enable JSON body parsing
 app.use(express.urlencoded({ extended: true })); // Enable URL-encoded body parsing
 
@@ -109,7 +125,33 @@ app.put('/users/:id', async (req, res) => {
     res.status(500).json({ error: 'Failed to update user.' });
   }
 });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 /* 
+const productData = [];
+
+app.post('/api/add_product', (req, res) => {
+     console.log("Result",req.body);
+     const pdata = { 
+      "id": productData.length+1,
+      "pname": req.body.pname,
+      "price": req.body.price,
+      "desc": req.body.pdesc
+     };
+})
+
+productData.push(pdata);
+console.log("Final",pdata);
+
+res.status(200).send({
+    "status_code": 200,
+    "status_message": "Product added successfully",
+    "prodcuct": pdata
+}) */
+/* ,
 app.get('/users/search', async (req, res) => {
     const { name } = req.query; // Extract search term from query parameters
   
@@ -137,10 +179,6 @@ app.get('/users/search', async (req, res) => {
   });
 
  */
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
 
 
 /* //middleware
